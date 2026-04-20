9eb0f92fbec63ac80d4777cecd60a5029ec4afdd
大山 <dashan@dashandeMacBook-Pro.local>
2026-04-20 15:41:46 +0800

    new change

--- projects/src/app.ts
+++ projects/src/app.ts
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 export function createServerClient() {
+//   const cookieStore = cookies()
+//   console.log('SUPABASE_URL:', process.env.SUPABASE_URL)
   console.log('NEXT_PUBLIC_SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
 //   console.log('SUPABASE_ANON_KEY:', process.env.SUPABASE_ANON_KEY)
   console.log('NEXT_PUBLIC_SUPABASE_ANON_KEY', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
